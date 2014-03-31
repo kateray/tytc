fabe6410f9b645b4805c0cc10210c56d43e4bc7a
Kate Ray <email@kateray.net>
2014-03-31 17:23:06 -0400

    404 if no lang found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActionController::RoutingError, :with => :render_404
+
+  private
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def render_404(exception = nil)
+    if exception
+      # do something
+    end
+
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+
 end
--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,7 +1,7 @@
 class LanguagesController < ApplicationController
 
   def show
-    @language = Language.find_by_slug(params[:language])
+    @language = Language.find_by_slug(params[:language]) || not_found
     @link = Link.new
   end
 
